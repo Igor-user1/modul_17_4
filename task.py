7b12a3958295dad446a3659287810a18ad44ace0
Igor-user1 <xadeev.igor@icloud.com>
2025-01-26 07:08:54 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,19 +1,85 @@
-from backend.db import Base
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
-from sqlalchemy.orm import relationship
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from db_depends import get_db
+from typing import Annotated
+from models.task import Task
 from models.user import User
+from schemas import CreateTask, UpdateTask
+from sqlalchemy import insert, text
 
 
-class Task(Base):
-    __tablename__ = 'task'
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    content = Column(String)
-    priority = Column(Integer, default=0)
-    completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    slug = Column(String, unique=True, index=True)
-    user = relationship('User', back_populates='tasks')
+router = APIRouter(prefix='/task', tags=['task'])
+DBSession = Annotated[Session, Depends(get_db)]
 
-from sqlalchemy.schema import CreateTable
-print(CreateTable(Task.__table__))
+
+@router.get('/')
+async def all_task(db: DBSession):
+    tasks = db.execute(text("SELECT id, title, content, priority, user_id, slug FROM task"))
+    list_of_all_tasks = []
+    for task in tasks:
+        task = {'id': task.id, 'title': task.title, 'content': task.content,
+                'priority': task.priority,
+                'user_id': task.user_id, 'slug': task.slug}
+        list_of_all_tasks.append(task)
+    return list_of_all_tasks
+
+@router.get('task_id')
+async def task_id(db: DBSession, id_task: int):
+    tasks = db.execute(text("SELECT id, title, content, priority, user_id, slug FROM task"))
+    list_of_all_tasks = []
+    for task in tasks:
+        if task.id == id_task:
+            task = {'id': task.id, 'title': task.title, 'content': task.content,
+                'priority': task.priority,
+                'user_id': task.user_id, 'slug': task.slug}
+            list_of_all_tasks.append(task)
+            return list_of_all_tasks
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Task was not found')
+
+@router.post('/create')
+async def create_task(db: DBSession, task_create: CreateTask, user_id: int):
+    user = db.execute(text("SELECT * FROM users WHERE id = :user_id"),
+                      {'user_id': user_id}).fetchone()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    db.execute(insert(Task).values(title=task_create.title,
+                                   content=task_create.content,
+                                   priority=task_create.priority,
+                                   user_id=user_id))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
+
+@router.put('/update')
+async def update_task(db: DBSession, id_task: int, task_update: UpdateTask):
+    task = db.execute(text("SELECT * FROM task WHERE id = :id"),
+                      {'id': id_task}).fetchone()
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    update_query = text(
+        'UPDATE task SET title = :title, content = :content, priority = :priority '
+        'WHERE id = :id')
+    db.execute(update_query,
+               {'id': id_task, 'title': task_update.title,
+                'content': task_update.content, 'priority': task_update.priority}, )
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'task update successful!'}
+
+@router.delete('/delete')
+async def delete_task(db: DBSession, id_task: int):
+    task = db.execute(text("SELECT * FROM task WHERE id = :id"),
+                      {'id': id_task}).fetchone()
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    delete_query = text(
+        'DELETE FROM task WHERE id = :id')
+    db.execute(delete_query,
+               {'id': id_task},)
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'task delete successful!'}
--- user.py
+++ user.py
@@ -1,20 +1,87 @@
-from backend.db import Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from db_depends import get_db
+from typing import Annotated
+from models.user import User
+from schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, text
+
+router = APIRouter(prefix='/user', tags=['users'])
+DBSession = Annotated[Session, Depends(get_db)]
+
+@router.get('/')
+async def all_users(db: DBSession):
+    users = db.execute(text("SELECT id, username, firstname, lastname, age FROM users"))
+    list_of_all_users = []
+    for user in users:
+        user = {'id': user.id,'username': user.username, 'firstname': user.firstname,
+                'lastname': user.lastname, 'age': user.age}
+        list_of_all_users.append(user)
+    return list_of_all_users
 
 
+@router.post('/create')
+async def create_user(db: DBSession, create_new_user: CreateUser):
+    db.execute(insert(User).values(username=create_new_user.username,
+                                   firstname=create_new_user.firstname,
+                                   lastname=create_new_user.lastname,
+                                   age=create_new_user.age))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
 
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    firstname = Column(String)
-    lastname = Column(String)
-    age = Column(Integer)
-    slug = Column(String, unique=True, index=True)
-    task = relationship('Task', back_populates='user')
+@router.get('/user_id')
+async def user_by_id(db: DBSession, user_id: int):
+    users = db.execute(text("SELECT id, username, firstname, lastname, age FROM users"))
+    list_of_all_users = []
+    for user in users:
+        if user.id == user_id:
+            user = {'id': user.id, 'username': user.username, 'firstname': user.firstname,
+                        'lastname': user.lastname, 'age': user.age}
+            list_of_all_users.append(user)
+            return list_of_all_users
+    raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+
+@router.get('/user_id/tasks')
+async def task_by_user_id(db: DBSession, user_id: int):
+    task = db.execute(text("SELECT * FROM task WHERE user_id = :user_id"),
+                      {'user_id': user_id}).fetchone()
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    return [task.id, task.title, task.content, task.priority]
+
+@router.put('/update')
+async def update_user(db: DBSession,
+                      user_update: UpdateUser, user_id: int):
+    user = db.execute(text("SELECT * FROM users WHERE id = :id"),
+                       {'id': user_id}).fetchone()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    update_query = text(
+        'UPDATE users SET firstname = :firstname, lastname = :lastname, age = :age WHERE id = :id')
+    db.execute(update_query,
+               {'id': user_id, 'firstname': user_update.firstname,
+                'lastname': user_update.lastname, 'age': user_update.age},)
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'User update successful!'}
 
 
-from sqlalchemy.schema import CreateTable
-print(CreateTable(User.__table__))
+@router.delete('/delete')
+async def delete_user(db: DBSession, user_id):
+    user = db.execute(text("SELECT * FROM users WHERE id = :id"),
+                      {'id': user_id}).fetchone()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    delete_query = text(
+        'DELETE FROM users WHERE id = :id')
+    db.execute(delete_query,
+               {'id': user_id}, )
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'User update successful!'}
